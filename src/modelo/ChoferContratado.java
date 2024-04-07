4c6bf0572a34dcc2f5d679f4ae8235e450a5adc5
SofiaIPalladino <sofia.palladino.2001@gmail.com>
2024-04-07 16:58:57 -0300

    commit inicial

---
+++ src/modelo/ChoferContratado.java
@@ -0,0 +1,12 @@
+package modelo;
+
+public class ChoferContratado extends Chofer {
+    public ChoferContratado(String dni, String nombre, double sueldoBasico, double plusAntiguedad, double plusHijos, double aportes, LocalDate fechaIngreso) {
+        super(dni, nombre, sueldoBasico, plusAntiguedad, plusHijos, aportes, fechaIngreso);
+    }
+
+    @Override
+    public double calcularSueldo() {
+        // implementación del calculo del sueldo para un chofer contratado
+    }
+}
---
+++ src/modelo/ChoferPermanente.java
@@ -0,0 +1,12 @@
+package modelo;
+
+public class ChoferPermanente extends Chofer {
+    public ChoferPermanente(String dni, String nombre, double sueldoBasico, double plusAntiguedad, double plusHijos, double aportes, LocalDate fechaIngreso) {
+        super(dni, nombre, sueldoBasico, plusAntiguedad, plusHijos, aportes, fechaIngreso);
+    }
+
+    @Override
+    public double calcularSueldo() {
+        // Implementación del cálculo del sueldo para un chofer permanente
+    }
+}
