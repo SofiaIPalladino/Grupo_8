6e9cc20f87fb8a890f615178a755a9a3ae3d14d1
AgustinChazarreta <164357035+chazarretaAgustin@users.noreply.github.com>
2024-04-18 21:11:37 -0300

    initial commit

--- src/modelo/Auto.java
+++
@@ -1,7 +0,0 @@
-package modelo;
-
-public class Auto extends Vehiculo {
-    public Auto(String patente, int cantMaxPasajeros, boolean petFriendly, boolean baúl) {
-        super(patente, cantMaxPasajeros, petFriendly, baúl);
-    }
-}
--- src/module-info.java
+++ src/module-info.java
@@ -4,5 +4,5 @@
 /**
  * 
  */
-module TPFINAL {
+module pruebaTP {
 }
