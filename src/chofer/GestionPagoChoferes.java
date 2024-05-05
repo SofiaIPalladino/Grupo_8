4d8b9e3ebc443eeb7778e75c804815b8053182c1
SofiaIPalladino <sofia.palladino.2001@gmail.com>
2024-05-05 12:48:25 -0300

    actualizo gestion

--- src/chofer/GestionPagoChoferes.java
+++ src/chofer/GestionPagoChoferes.java
@@ -1,5 +1,19 @@
 package chofer;
 
+import sistema.Empresa;
+import usuario.Usuario;
+
 public class GestionPagoChoferes {
+	
+	Empresa e=Empresa.getInstance();
+	
+	public void calculoPagoChoferes(Usuario usuario) {
+		if (usuario.isAdmin()) {
+			
+		}else {
+			System.out.println("Solo el administrador puede pagar los salarios");	
+		}
+		
+	}
 
 }
--- src/puntajes/GestionPuntajes.java
+++ src/puntajes/GestionPuntajes.java
@@ -1,25 +1,29 @@
 package puntajes;
 
 import sistema.Empresa;
+import usuario.Usuario;
 import chofer.Chofer;
 import java.util.List;
 
 public class GestionPuntajes {
-    public static void actualizarPuntajes() {
-        Chofer chofermax=null;
-        double maxDistancia = 0;
-        Empresa empresa=Empresa.getInstance();
-        List<Chofer> choferes=empresa.getChoferes();
-        for (int i = 0; i <  choferes.size(); i++) {
-        	Chofer c=choferes.get(i);
-        	c.agregaPuntaje(c.cantViajes()*5);
-            if (c.getKm() > maxDistancia) {
-                maxDistancia = c.getKm();
-                chofermax=c;
+    public static void actualizarPuntajes(Usuario usuario) {
+    	if (usuario.isAdmin()) {
+    		Chofer chofermax=null;
+    		double maxDistancia = 0;
+    		Empresa empresa=Empresa.getInstance();
+    		List<Chofer> choferes=empresa.getChoferes();
+    		for (int i = 0; i <  choferes.size(); i++) {
+    			Chofer c=choferes.get(i);
+    			c.agregaPuntaje(c.cantViajes()*5);
+    			if (c.getKm() > maxDistancia) {
+    				maxDistancia = c.getKm();
+    				chofermax=c;
                 
-            }
-        }
-        if (chofermax!=null)
-        	chofermax.agregaPuntaje(15);
+    			}
+    		}
+    		if (chofermax!=null)
+    			chofermax.agregaPuntaje(15);
+    	}else 
+    		System.out.println("Solo el administrador puede actualizar los puntajes");	  	
     }
 }
